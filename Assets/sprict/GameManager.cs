9efe9f48f4f45ff2e5efae4c5df65cf157f9029e
SHUNSHUN-0031 <saruwatarishun15@gmail.com>
2022-09-03 20:32:59 +0900

    なんかよくわからんバグ解消

--- Assets/sprict/GameManager.cs
+++ Assets/sprict/GameManager.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField, Tooltip("スタート")]
+    Image start;
+    [SerializeField, Tooltip("カウントテキスト")]
+    Text startText;
+    public float s;
     // Start is called before the first frame update
     void Start()
     {
